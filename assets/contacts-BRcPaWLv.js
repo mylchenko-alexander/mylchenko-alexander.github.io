b85a09181f5c8595e46bc812d00e6836181b50a8
Alexander M <mylchenko.alexandr@gmail.com>
2026-05-08 14:36:29 +0300

    update(s)

--- assets/contacts-BRcPaWLv.js
+++
@@ -1,6 +0,0 @@
-const t=`{\r
-  "context": [\r
-    [ "email", "mailto:mylchenko.alexandr@gmail.com" ],\r
-    [ "telegram", "https://t.me/cyberdelika" ]\r
-  ]\r
-}`;export{t as default};
---
+++ assets/contacts-BrS3TzwF.js
@@ -0,0 +1,15 @@
+const n=`{\r
+  "context": {\r
+    "contacts.header": {\r
+      "Email": "mailto:mylchenko.alexandr@gmail.com",\r
+      "Telegram": "https://t.me/cyberdelika"\r
+    },\r
+    "social.header": {\r
+      "LinkedIn": "https://www.linkedin.com/in/mylchenko"\r
+    },\r
+    "hobby.header": {\r
+      "Bandcamp": "https://psychoknox.bandcamp.com",\r
+      "YouTube": "https://www.youtube.com/@yoga-gym"\r
+    }\r
+  }\r
+}`;export{n as default};
